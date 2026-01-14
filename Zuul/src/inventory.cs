59c9fbc9030c3356c510df399febecc61f6882e6
iliauser2345 <124836886+iliauser2345@users.noreply.github.com>
2026-01-14 22:02:03 +0100

    Inventory is a thing(Details in the changelog.txt)

---
+++ Zuul/src/inventory.cs
@@ -0,0 +1,65 @@
+class Inventory
+{
+    // fields
+    private int maxWeight;
+    private Dictionary<string, Item> items;
+    // constructor
+    public Inventory(int maxWeight)
+    {
+        this.maxWeight = maxWeight;
+        this.items = new Dictionary<string, Item>();
+    }
+    // methods
+    public bool Put(string itemName, Item engagedItem)
+    {
+        int weight=engagedItem.Weight;
+        bool succes; 
+        if (weight <= FreeWeight())
+        {
+            items.Add(itemName,engagedItem);
+            succes=true;
+        }
+        else
+        {
+            succes=false;
+        }
+        return succes;
+    }
+    public Item Get(string itemName)
+    {
+        if (items.ContainsKey(itemName) == true)
+        {
+            Item gotItem=items[itemName];
+            items.Remove(itemName);
+            return gotItem;
+        }
+        else
+        {
+            return null;
+        }
+    // TODO implementeer:
+    // Zoek Item in de Dictionary
+    // Verwijder Item uit Dictionary (als gevonden)
+    // Return Item of null
+    }
+    public int TotalWeight()
+    {
+        int total;
+        int i;
+        for(
+            total=0,
+            i=0;
+            i<items.Count;
+            i++
+        )
+        {
+            Item LoopThrItem=items.Values.ElementAt(i);
+            total=+LoopThrItem.Weight;
+        }
+        return total;
+    }
+    public int FreeWeight()
+    {
+        return maxWeight-TotalWeight();
+    }
+}
